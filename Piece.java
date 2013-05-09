662c748d05b0e78d43500e4b682fd4bc9090bcd4
Anirudh <mathukumillia@s.dcsdk12.org>
2013-05-09 08:18:04 -0600

    finished rook

--- Piece.java
+++ Piece.java
@@ -1,6 +1,7 @@
-public class Piece{
+public abstract class Piece{
 	
 	private Point location;
+	private String color;
 
 	/**
 	 * move
@@ -11,10 +12,18 @@ public class Piece{
 	}
 
 	/**
-	 * getter
+	 * getters and setters
 	 */
 	public Point getLoc(){
 		return location;
+	}
+
+	public void setColor(String c){
+		color = c;
+	}
+
+	public String getColor(){
+		return color;
 	}
 
 }
--- Rook.java
+++ Rook.java
@@ -1,3 +1,16 @@
 public class Rook extends Piece{
 	
+	public Rook(String color, int x, int y){
+		setColor(color);
+		getLoc() = new Point(x,y);
+	}
+
+	public void makeMove(int x, int y){
+		if(x == (int)(getLoc().getX()) || y == (int)(getLoc().getY()) ){
+			move(x,y);
+		}else{
+			return;
+		}
+	}
+
 }
