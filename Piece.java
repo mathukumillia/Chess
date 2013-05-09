27c25e2599491e92bcfb817367aa333781d80c89
Anirudh <mathukumillia@s.dcsdk12.org>
2013-05-09 08:09:24 -0600

    finish piece

--- Piece.java
+++ Piece.java
@@ -1,3 +1,20 @@
 public class Piece{
 	
+	private Point location;
+
+	/**
+	 * move
+	 * 		moves the piece to the specified location
+	 */
+	public void move(int x, int y){
+		location.setLocation(x,y);
+	}
+
+	/**
+	 * getter
+	 */
+	public Point getLoc(){
+		return location;
+	}
+
 }
