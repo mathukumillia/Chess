76271899445de0c686a6007a09f89484ba95fea0
Anirudh <mathukumillia@s.dcsdk12.org>
2013-05-17 09:10:59 -0600

    sort of works-not really

--- Board.java
+++ Board.java
@@ -1,20 +1,26 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.GridLayout;
 
 public class Board extends JPanel{
 
 	private Game ga;
+	private Dimension d;
 
 	public Board(){
+		super(new GridLayout(8,8));
 		ga = new Game();
+		d = new Dimension(50,50);
 	}
 
 	public void paint(Graphics g){
 		Team[] teams = ga.getTeams();
+		Piece[] pieces;
 		for(Team t : teams){
-			for(Piece p : t.getPieces()){
-				p.draw(g,new Dimension(300,400));
-			}
+			pieces = t.getPieces();
+			pieces[0].draw(g, d);
+			Tile tile = new Tile(pieces[0]);
+			this.addLayoutComponent("First Square", tile);
 		}
 	}
 
