15860f09e8d5487bcd4b505d630d586d7b529a40
Anirudh <mathukumillia@s.dcsdk12.org>
2013-05-21 08:16:48 -0600

    pieces draw

--- Board.java
+++ Board.java
@@ -5,23 +5,27 @@ import java.awt.GridLayout;
 public class Board extends JPanel{
 
 	private Game ga;
-	private Dimension d;
+	private Team[] teams;
+	private Piece[] pieces;
+	private PiecePanel[] panels;
 
 	public Board(){
 		super(new GridLayout(8,8));
 		ga = new Game();
-		d = new Dimension(50,50);
+		teams = ga.getTeams();
+		this.setVisible(true);
+		for(Team t : teams){
+			pieces = t.getPieces();
+			panels = new PiecePanel[pieces.length];
+			for(int i = 0; i<pieces.length; i++){
+				panels[i] = new PiecePanel(pieces[i]);
+				this.add(panels[i]);
+			}
+		}
 	}
 
 	public void paint(Graphics g){
-		Team[] teams = ga.getTeams();
-		Piece[] pieces;
-		for(Team t : teams){
-			pieces = t.getPieces();
-			pieces[0].draw(g, d);
-			Tile tile = new Tile(pieces[0]);
-			this.addLayoutComponent("First Square", tile);
-		}
+		super.paint(g);
 	}
 
 }
