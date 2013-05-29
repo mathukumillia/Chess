dc811240b7151b6ab39fdd993967427032886bd5
Matthew Price <doggy1199@yahoo.com>
2013-05-29 08:52:27 -0600

    Will didn't do crap

--- PiecePanel.java
+++ PiecePanel.java
@@ -16,7 +16,7 @@ public class PiecePanel extends JPanel {
 	public PiecePanel(Piece p){
 		super(new GridLayout(1,1));
 		this.p = p;
-		this.setVisible(true); 
+		this.setVisible(true);
 	}
 
 	public void paint(Graphics g){
